28588cd1b0adec864c33b2b02d630ef48c5743b8
wt5n <wt5n@users.noreply.github.com>
2021-06-12 16:24:18 +0300

    Let's start the Rumble

--- src/main/java/ru/school21/Puzzle.java
+++ src/main/java/ru/school21/Puzzle.java
@@ -1,5 +1,7 @@
 package ru.school21;
 
+import java.util.PriorityQueue;
+
 import lombok.Data;
 
 @Data
@@ -9,6 +11,11 @@ public class Puzzle {
 	private int sizeComplexity;
 	private int numberOfMoves;
 	private StringBuilder solution;
+	private PriorityQueue<String> map;
+	private String testUnsolve = "# This puzzle is unsolvable\n" + "4\n" + "12  1  4 15 \n" + " 0 13  6  7 \n" +
+			" 9  2 11  5 \n" + " 3  8 14 10 ";
+	private String testSolve = "# This puzzle is solvable\n" + "4\n" + "10  7  1 12 \n" + "13 14  3 11 \n" +
+			" 6  9  8  2 \n" + " 5 15  0  4 ";
 
 	public Puzzle(String puzzleString) {
 		this.puzzleString = puzzleString;
@@ -19,8 +26,10 @@ public class Puzzle {
 	}
 
 	public String solvePuzzle() {
-
-		return "NoTest";
+		return "Test";
 	}
 
+	private void convertToList() {
+//		map = puzzleString;
+	}
 }
